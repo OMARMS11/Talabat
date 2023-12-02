b261676fd49a3b9c97dcd4cfe1ba474691442278
OMARMS11 <omarmustafa2017@gmail.com>
2023-12-02 21:59:47 +0200

    make some functions in Admin

--- Admin.java
+++ Admin.java
@@ -1,6 +1,38 @@
 package Talabat
 
-public class Admin
-        {
+public class Admin extends User {
+public  void deleteResturant(){
 
-        }
+}
+
+public  void add resturant(){
+
+}
+
+
+public  void addItemToMenu()
+{
+
+}
+
+public void removeItemFromMenu()
+{
+
+}
+
+ public  void displayResturant()
+ {
+
+ }
+
+public  void updateItemPrice()
+{
+
+}
+
+public  void displayResturantMenu(Resturant)
+{
+
+}
+
+}
